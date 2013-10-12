f9496aa44df33cf4463d41b6bd99606a878c5d83
Wenbo Z <bozi1984@gmail.com>
2013-10-12 14:48:50 -0700

    First Application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Crashable::Application.config.secret_token = 'd3251159733aa6fc817fad377f23cd3252579ba7b4498421ef18a10daceeeb9bbc278c6a116021d75679e0bc35d4aa914ab212caf9232a5d400c93a8a7ff3256'
